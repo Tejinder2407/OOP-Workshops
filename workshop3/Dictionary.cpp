bc0c84936f36f4f0861baa5a6387ea3a4eeeff7a
Tejinder singh <122826760+Tejinder2407@users.noreply.github.com>
2023-05-28 13:43:17 -0400

    Update Dictionary.cpp

--- workshop3/Dictionary.cpp
+++ workshop3/Dictionary.cpp
@@ -1,12 +1,4 @@
-/*
-    ==================================================
-    Workshop 03: OOP435ZCC
-    ==================================================
-    Name   : Tejinder Singh
-    ID     : 129121216
-    Email  : ktejinder-singh@myseneca.ca
-    Section: ZCC
-*/
+
 
 #include "Dictionary.h"
 namespace sdds
