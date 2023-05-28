3c3a55db07cf02806c3c753f95e45d1b153cabe8
Tejinder singh <122826760+Tejinder2407@users.noreply.github.com>
2023-05-28 13:43:40 -0400

    Update Queue.h

--- workshop3/Queue.h
+++ workshop3/Queue.h
@@ -1,12 +1,4 @@
-/*
-    ==================================================
-    Workshop 03: OOP435ZCC
-    ==================================================
-    Name   : Tejinder Singh
-    ID     : 129121216
-    Email  : ktejinder-singh@myseneca.ca
-    Section: ZCC
-*/
+
 
 #ifndef SDDS_QUEUE_H_
 #define SDDS_QUEUE_H_
